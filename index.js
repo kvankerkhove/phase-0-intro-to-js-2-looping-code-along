51b5a437389e5e3be36e19a6a4ba5be55d02b324
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-05 16:45:09 -0400

    commit tree 4c0fa07800ce7b9e4f884b40faa0ba0fc0997a31

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+function writeCards(array, event) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(positiveInt) {
+    let counter = positiveInt;
+    while (counter >= 0) {
+        console.log(counter);
+        counter --;
+    }
+}
